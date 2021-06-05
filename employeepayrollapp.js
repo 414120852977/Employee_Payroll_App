f555cf3b66ce42f971995ed2835a17901e7cff45
Ashok Mane <ashokmane81800@gmail.com>
2021-06-05 15:45:06 +0530

    [ashok] refractor uc8 uc9 and uc10

--- employeepayrollapp.js
+++ employeepayrollapp.js
@@ -5,3 +5,34 @@
         salary.addEventListener('input', function(){
             output.textContent = salary.value;
         });
+
+        /*UC 10 Validating name*/
+      /*JS code for the validation of name*/
+      const text = document.querySelector('#name');       
+        const textError = document.querySelector('.text-error');
+        text.addEventListener('input',function(){
+            /*Regex for name*/
+            let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$');      
+            if(nameRegex.test(text.value))
+                textError.textContent = "";
+            else textError.textContent = "Name is incorrect";
+        });    
+
+        window.addEventListener('DOMContentLoaded', (event) => {
+    const name = document.querySelector('#name');
+    const textError = document.querySelector('.text-error');
+    name.addEventListener('input', function () {
+        if (name.value.length == 0) {
+            textError.textContent = "";
+            return;
+        }
+        try {
+            (new EmployeePayrollData()).name = name.value;;
+            textError.textContent = "";
+        } catch (e) {
+            textError.textContent = e;
+        }
+    });
+
+
+}); 
