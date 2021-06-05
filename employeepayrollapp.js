65c6e5dd563aaaaa1fdc4681a829ab64be13b3a6
Ashok Mane <ashokmane81800@gmail.com>
2021-06-05 15:27:48 +0530

    [ashok] added js file to slide range of a salary

---
+++ employeepayrollapp.js
@@ -0,0 +1,7 @@
+/*JS code for slider*/
+    const salary = document.querySelector('#salary');       
+        const output = document.querySelector('.salary-output');
+        output.textContent = salary.value;
+        salary.addEventListener('input', function(){
+            output.textContent = salary.value;
+        });
